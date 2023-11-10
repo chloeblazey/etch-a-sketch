414bb76b2ffbad323088d7e9fd489353182c470b
chloeblazey <chloeblazey@gmail.com>
2023-11-10 00:46:26 -0800

    Add basic drawing functionality
    The cells of the grid change color like an etch-a-sketch on mouseover.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const GRID_SIZE = 16;
 /* Creates the 16x16 grid that will become the etch-a-sketch board */
 const gridContainer = document.querySelector("#grid-container");
 
+/* Adds the "etched" class to an element, changing its color*/
+function onMouseEnter(e) {
+  e.target.className += " etched";
+}
+
 for (let i = 0; i < GRID_SIZE; i++) {
   const col = document.createElement("div")
   col.setAttribute("id", "col-" + i);
@@ -13,7 +18,9 @@ for (let i = 0; i < GRID_SIZE; i++) {
     const cell = document.createElement("div")
     cell.setAttribute("id", "row-" + j)
     cell.setAttribute("class", "flex-cell");
-    cell.textContent = i + ", " + j;
+
+    // create event listener for each cell
+    cell.addEventListener("mouseenter", onMouseEnter)
     col.appendChild(cell);
   }
   gridContainer.appendChild(col);
