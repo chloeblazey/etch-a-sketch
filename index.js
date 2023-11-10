85e9285abb0382ef6e37393d6525e8c90c03df83
chloeblazey <chloeblazey@gmail.com>
2023-11-10 00:09:38 -0800

    Style the grid to display as a square

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const GRID_SIZE = 16;
 
 /* Creates the 16x16 grid that will become the etch-a-sketch board */
 const gridContainer = document.querySelector("#grid-container");
+
 for (let i = 0; i < GRID_SIZE; i++) {
   const col = document.createElement("div")
   col.setAttribute("id", "col-" + i);
   col.setAttribute("class", "flex-col");
+
   for (let j = 0; j < GRID_SIZE; j++) {
     const cell = document.createElement("div")
     cell.setAttribute("id", "row-" + j)
     cell.setAttribute("class", "flex-cell");
+    cell.textContent = i + ", " + j;
     col.appendChild(cell);
   }
   gridContainer.appendChild(col);
