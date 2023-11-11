7e51519454e4abff4dd4218a4a4308ecaa463343
chloeblazey <chloeblazey@gmail.com>
2023-11-11 14:43:31 -0800

    Complete the form to customize grid density
    This commit allows the user to update the grid size using a form.
    The commit also includes minor styling changes to the grid, so that it
    stays the same size as grid density increases.

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-/* Defines the width and height of our grid */
-const GRID_SIZE = 16; 
-
+const gridContainer = document.querySelector("#grid-container");
 
 /* Adds the "etched" class to an element, changing its color*/
 function onMouseEnter(e) {
   e.target.className += " etched";
 }
 
-const gridContainer = document.querySelector("#grid-container");
+/* Creates the size x size grid that will become the etch-a-sketch board */
+function createGrid(gridSize = 16) {
+  gridContainer.replaceChildren();
+  for (let i = 0; i < gridSize; i++) {
+    const col = document.createElement("div")
+    col.setAttribute("id", "col-" + i);
+    col.setAttribute("class", "flex-col");
 
-/* Creates the 16x16 grid that will become the etch-a-sketch board */
-for (let i = 0; i < GRID_SIZE; i++) {
-  const col = document.createElement("div")
-  col.setAttribute("id", "col-" + i);
-  col.setAttribute("class", "flex-col");
+    for (let j = 0; j < gridSize; j++) {
+      const cell = document.createElement("div")
+      cell.setAttribute("id", "row-" + j)
+      cell.setAttribute("class", "flex-cell");
 
-  for (let j = 0; j < GRID_SIZE; j++) {
-    const cell = document.createElement("div")
-    cell.setAttribute("id", "row-" + j)
-    cell.setAttribute("class", "flex-cell");
-
-    // create event listener for each cell
-    cell.addEventListener("mouseenter", onMouseEnter)
-    col.appendChild(cell);
-  }
-  gridContainer.appendChild(col);
-}
+          // create event listener for each cell
+      cell.addEventListener("mouseenter", onMouseEnter)
+      col.appendChild(cell);
+    }
+    gridContainer.appendChild(col);
+}}
 
 /* Updates the number of cells in the grid */
 function updateGrid(e) {
-  alert(e.target.elements[0].value);
+  e.preventDefault();
+  let n = e.target.elements[0].value;
+  createGrid(n);
 }
 
-/* */
+/* Attaches an event listener to the form submit button */
 const form = document.querySelector("#grid-size-form");
 form.addEventListener("submit", updateGrid);
+
+createGrid();
